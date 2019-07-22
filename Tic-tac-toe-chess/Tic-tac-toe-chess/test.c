2a6e8256eabfcd7a84052a16bf55b922c50ec242
Luckyboy1024 <2819549538@qq.com>
2019-07-22 18:13:25 +0800

    test.c  19_07_22的修改 game.c  19_07_22的修改 game.h  19_07_22的修改

--- Tic-tac-toe-chess/Tic-tac-toe-chess/test.c
+++ Tic-tac-toe-chess/Tic-tac-toe-chess/test.c
@@ -1,6 +1,46 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-#if 0
+#include "game.h"
 
 
-#endif
+void menu()
+{
+	printf("□□□□□□□□□□□□□□□□□□\n");
+	printf("□		1. 新游戏                □\n");
+	printf("□		0. 退出游戏		        □\n");
+	printf("□□□□□□□□□□□□□□□□□□\n");
+	printf("□□□□□□□□□□□□□□□□□□\n");
+	printf("\n\n\n\n");
+	printf("请输入:>_");
+}
+
+
+void menu1()
+{
+	printf("*************************************\n");
+	printf("**			1.先手				**\n");
+	printf("**			2.后手				**\n");
+	printf("**			0.退出				**\n");
+	printf("*************************************\n");
+}
+
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		scanf(input);
+		switch (input)
+		{
+		case 1:
+			menu1();
+			break;
+		case 2:
+		case 0:
+			return 0;
+			break;
+		default:
+			break;
+		}
+	} while (input);
+	return 0;
+}
